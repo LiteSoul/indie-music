de0678814785727a2a3780f1ed49c8d0e02cdeb9
LiteSoul <exorcismo@gmail.com>
2023-08-29 10:57:33 -0300

    typescripting map data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GRID_DATA_ITEMS } from './data'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          {GRID_DATA_ITEMS.map((gridDataItem) => {
+            const {
+              id,
+              attributes,
+            }: { id: number; attributes: { text: string } } = gridDataItem
+            return <div key={id}>{attributes.text}</div>
+          })}
+        </div>
+        <br />
+        {children}
+      </body>
     </html>
   )
 }
